527cabc1e85c73fae2461fb9db0a223fb843b872
DenisWritesCode <DWC@Denis-MBP.local>
2023-09-21 14:19:08 +0300

    Done with 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const phoneBookUsers = [
+let phoneBookUsers = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -28,26 +28,34 @@ app.use(express.json());
 
 // Fetch all users
 app.get("/api/persons", (request, response) => {
-    response.json(phoneBookUsers);
+  response.json(phoneBookUsers);
 });
 
 // Fetch number of users and time of request
-app.get('/info', (request, response) => {
-    const timeOfReceivingRequest = new Date();
-    const responseString = `<p>Phonebook has info for ${phoneBookUsers.length} people.<p>${timeOfReceivingRequest}</p>`
-    response.send(responseString);
+app.get("/info", (request, response) => {
+  const timeOfReceivingRequest = new Date();
+  const responseString = `<p>Phonebook has info for ${phoneBookUsers.length} people.<p>${timeOfReceivingRequest}</p>`;
+  response.send(responseString);
 });
 
 // Fetch single user
 app.get("/api/persons/:id", (request, response) => {
-    const userID = Number(request.params.id);
-    const user = phoneBookUsers.find(person => person.id === userID);
+  const userID = Number(request.params.id);
+  const user = phoneBookUsers.find((person) => person.id === userID);
 
-    if(user) {
-        response.json(user);
-    } else {
-        response.status(404).end();
-    }
+  if (user) {
+    response.json(user);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// Delete single user
+app.delete("/api/persons/:id", (request, response) => {
+  const userID = Number(request.params.id);
+  phoneBookUsers = phoneBookUsers.filter((person) => person.id !== userID);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
